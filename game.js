c7f56fe0146fab9ab8f97520a9cb1408cd506c2f
Peter Seibel <peter@gigamonkeys.com>
2022-01-05 21:26:37 -0800

    Rewrapping comments and tidying html.

--- game.js
+++ game.js
@@ -1,7 +1,8 @@
-// This file contains the code for the clicker game that is required by the framework
-// defined in clicker.js. The only requirements are that you define three functions:
-// `setup`, `click`, and `update`. See the code in clicker.js for more discussion 
-// of how they are used. Simple version of all three functions are defined below.
+// This file contains the code for the clicker game that is required
+// by the framework defined in clicker.js. The only requirements are
+// that you define three functions: `setup`, `click`, and `update`.
+// See the code in clicker.js for more discussion of how they are
+// used. Simple version of all three functions are defined below.
 
 let coins = 0;
 let houses = 0;
@@ -10,28 +11,20 @@ let clickValue = 1;
 let housePrice = 10;
 let houseIncomePerSecond = 1;
 
+// Called once by the clicker framework when the page is loaded.
 function setup() {
     document.getElementById("buy-house").onclick = e => buy("house");
 }
 
-function buy(what) {
-  if (what === "house") {
-    coins -= housePrice;
-    houses++;
-  } else {
-    console.log(`Don't know how to buy ${what}`)
-  }
-}
-
-// This function is called for each click, including each individual click of a
-// double or triple click.
+// Called for each mouse click.
 function click() {
   coins += clickValue;
 }
 
-// The function called for each step of the animation. We are told the number 
-// (likely fractional) of seconds that have passed since the last update. 
-// If this function ever returns false, the animation loop will stop.
+// Called for each step of the animation. We are told the number
+// (likely fractional) of seconds that have passed since the last
+// update. If this function ever returns false, the animation loop
+// will stop.
 function update(elapsed) {
 
   // Update values
@@ -48,8 +41,18 @@ function update(elapsed) {
   return true;
 }
 
-// Compute the number of coins of passive income (i.e. not from clicks) we
-// are currently earning per second.
+// Buy something. At the moment we only know how to buy a house.
+function buy(what) {
+  if (what === "house") {
+    coins -= housePrice;
+    houses++;
+  } else {
+    console.log(`Don't know how to buy ${what}`)
+  }
+}
+
+// Compute the number of coins of passive income (i.e. not from
+// clicks) we are currently earning per second.
 function incomePerSecond() {
   return houses * houseIncomePerSecond;
 }
